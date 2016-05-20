7d1d5f81773b37c3e8368a3a262bb45ba3b2a4e3
Davide Pedranz <davide.pedranz@gmail.com>
2016-05-20 17:12:23 +0200

    Use typescript for db.js

---
+++ src/db.ts
@@ -0,0 +1,33 @@
+/// <reference path="../typings/tsd.d.ts" />
+import redis = require('redis');
+import Promise = require('bluebird');
+
+// DB client
+export default class DB {
+
+    static USERS = 'user_ids';
+
+    client;
+
+    constructor(url?: string) {
+        let tmp = redis.createClient(url || process.env.REDIS_URL);
+        this.client = Promise.promisifyAll(tmp);
+    }
+
+    async drop() {
+        await this.client.flushall();
+    }
+
+    async addUser(id) {
+        await this.client.saddAsync(DB.USERS, id);
+    }
+
+    async deleteUser(id) {
+        await this.client.sremAsync(DB.USERS, id);
+    }
+
+    async getUsers() {
+        return await this.client.smembersAsync(DB.USERS);
+    }
+
+}
